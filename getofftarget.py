1dc489e8b8bf97aec35ec5a0818a8a3a93e0f959
be-garam <be.garam.dev@gmail.com>
2024-04-17 21:21:11 +0900

    ADD getting target input and output

--- getofftarget.py
+++ getofftarget.py
@@ -5,10 +5,26 @@ import pandas as pd
 import sys
 import argparse
 
-def make_input(gene_id, mismatch):
-    with open('gene_mismatch.txt', 'w') as file:
-        for gene_id, mismatch in zip(gene_id, mismatch):
-            file.write(f"{gene_id}\t{mismatch}\n")
+def make_input(gene_id, gene_ansembl_id, target_dict):
+    with open(f'{gene_ansembl_id}_input.txt', 'w') as file:
+        organism =  "Homo sapiens (GRCh38/hg38) - Human"
+        
+        #check this
+        path = f"path of {organism}"
+        
+        file.write(f"{path}\n")
+        for id, value in target_dict.items(): 
+            target = value[0]
+            mistmatch_count_list = value[1:]
+            for i in range(len(mistmatch_count_list)):
+                if mistmatch_count_list[i] != 0:
+                    file.write(f"{target} {i}\n")
+
+def make_output(gene_ansembl_id, offtarget_df):
+    with open(f'{gene_ansembl_id}_output.txt', 'w') as file:
+        for row in offtarget_df.itertuples(index=False):
+            file.write(' '.join(map(str, row)) + '\n')
+    
 
 def get_target_list(gene_id):
     target_dict = {}
@@ -16,14 +32,13 @@ def get_target_list(gene_id):
     response = requests.get(url)
     if str(response.status_code)[:2] == "20":
         data = response.json()
-        target_list = [i['id'] for i in data['targets']]
-        # print(target_list)
-        target_dict[gene_id] = target_list
+        for target in data['targets']:
+            target_dict[target["id"]] = [target["sequence"]] + target["offtarget_counts"]
     else:
         print(f"Error: Failed to retrieve gene list in gene_id: {gene_id}")
     return target_dict
 
-def get_offtarget_list(target_dict):
+def get_offtarget_list(gene_id, target_dict):
     total_gene_list = []
     total_target_list = []
     chromosome_list = []
@@ -33,23 +48,22 @@ def get_offtarget_list(target_dict):
     position_list = []
     mismatch_list = []
 
-    for gene_id, target_list in target_dict.items():
-        for target_id in target_list:
-            url = f"http://www.rgenome.net/cpf1-database/genes/{gene_id}/targets/{target_id}/offtargets/"
-            response = requests.get(url)
-            if str(response.status_code)[:2] == "20":
-                data = response.json()
-                for offtarget in data['offtargets']:
-                    total_gene_list.append(gene_id)
-                    total_target_list.append(target_id)
-                    chromosome_list.append(offtarget['chromosome'])
-                    sequence_list.append(offtarget['sequence'])
-                    region_list.append(offtarget['region'])
-                    strand_list.append(offtarget['strand'])
-                    position_list.append(offtarget['position'])
-                    mismatch_list.append(offtarget['mismatch_count'])
-            else:
-                print("Error: Failed to retrieve gene list in gene_id: {gene_id}")
+    for target_id in target_dict.keys():
+        url = f"http://www.rgenome.net/cpf1-database/genes/{gene_id}/targets/{target_id}/offtargets/"
+        response = requests.get(url)
+        if str(response.status_code)[:2] == "20":
+            data = response.json()
+            for offtarget in data['offtargets']:
+                total_gene_list.append(gene_id)
+                total_target_list.append(target_id)
+                chromosome_list.append(offtarget['chromosome'])
+                sequence_list.append(offtarget['sequence'])
+                region_list.append(offtarget['region'])
+                strand_list.append(offtarget['strand'])
+                position_list.append(offtarget['position'])
+                mismatch_list.append(offtarget['mismatch_count'])
+        else:
+            print("Error: Failed to retrieve gene list in gene_id: {gene_id}")
     
     df = pd.DataFrame({
         'gene_id': total_gene_list,
@@ -65,30 +79,33 @@ def get_offtarget_list(target_dict):
     return df
     
 
-def __main__(gene_id, mismatch):
+def __main__(gene_id):
     df = pd.read_csv('gene_data.csv')
     df.rename(columns={"Unnamed: 0": "id"}, inplace=True)
-    gene_ansembl_id = df.loc[df['gene_id'] == gene_id]["ensembl_id"]
+    # print(df.head())
+    gene_ansembl_id = df.loc[df['id'] == gene_id]["ensembl_id"].values[0]
     if gene_ansembl_id.empty:
         print("Error: Invalid gene_id")
         sys.exit(1)
     
     target_dict = get_target_list(gene_id)
-    
-    make_input(gene_id, mismatch)
+    # print(target_dict)
 
-    offtarget_df = get_offtarget_list(target_dict)
+    make_input(gene_id, gene_ansembl_id, target_dict)
+
+    offtarget_df = get_offtarget_list(gene_id, target_dict)
     print(offtarget_df.head())
-    offtarget_df.to_csv(f"offtarget_data_page{start_page_num}_{end_page_num}.csv", index=False)
+    offtarget_df.to_csv(f"{gene_ansembl_id}_output.csv", index=False)
+    make_output(gene_ansembl_id, offtarget_df)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script that gather Cpf1-Data base on page number")
     parser.add_argument("--gene", required=True, type=int)
-    parser.add_argument("--mismatch", required=False, type=int)
+    # parser.add_argument("--mismatch", required=False, type=int)
     args = parser.parse_args()
 
     gene_id = args.gene
-    mismatch = args.mismatch
+    # mismatch = args.mismatch
 
     # thining about using gene_data.csv is essential in this step
     # df = pd.read_csv('gene_data.csv')
@@ -99,9 +116,9 @@ if __name__ == "__main__":
     #     print("Error: Invalid gene_id")
     #     sys.exit(1)
 
-    if mismatch > 2:
-        print("Error: Mismatch value is too big")
-        sys.exit(1)
+    # if mismatch > 2:
+    #     print("Error: Mismatch value is too big")
+    #     sys.exit(1)
 
-    __main__(gene_id, mismatch)
+    __main__(gene_id)
 
